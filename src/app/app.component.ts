b60a4e081c81368cc860c965ace5fc2984a8195a
Aridan <aridanpantoja@gmail.com>
2024-12-02 17:54:52 -0300

    feat: add styles and html for filters and basic card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'cs-market';
+  title = 'cs-market'
+
+  filters = [
+    { label: 'Equipamento' },
+    { label: 'Pistolas' },
+    { label: 'Mid-Tier' },
+    { label: 'Rifles' },
+    { label: 'Granadas' },
+    { label: 'CT' },
+    { label: 'TR' },
+  ]
 }
