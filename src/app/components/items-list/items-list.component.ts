80169ca73ca78a346475ce2f3e55f58366804a59
Aridan <aridanpantoja@gmail.com>
2024-12-03 11:26:45 -0300

    feat(item): add item type and change items list

--- src/app/components/items-list/items-list.component.ts
+++ src/app/components/items-list/items-list.component.ts
@@ -1,27 +1,28 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { ApíService } from '../../services/apí.service';
+import { CommonModule } from '@angular/common'
+import { Component, OnInit } from '@angular/core'
+import { ApíService } from '../../services/apí.service'
+import { Item } from '../../models/item'
 
 @Component({
   selector: 'app-items-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './items-list.component.html',
-  styleUrl: './items-list.component.css'
+  styleUrl: './items-list.component.css',
 })
 export class ItemsListComponent implements OnInit {
-  products: any[] = []
+  products: Item[] = []
 
   constructor(private apiService: ApíService) {}
 
   ngOnInit(): void {
     this.apiService.getProducts().subscribe(
       (data) => {
-        this.products = data; // Atribuindo os dados recebidos ao array de produtos
+        this.products = data // Atribuindo os dados recebidos ao array de produtos
       },
       (error) => {
-        console.error('Erro ao carregar produtos', error);
-      }
-    );
+        console.error('Erro ao carregar produtos', error)
+      },
+    )
   }
 }
---
+++ src/app/models/item.ts
@@ -0,0 +1,8 @@
+export type Item = {
+  markethashname: string
+  slug: string
+  color: string
+  itemimage: string
+  itemgroup: string
+  pricelatest: number
+}
