06cc271c35484afd13e96cf0135fac8039395d51
Aridan <aridanpantoja@gmail.com>
2024-12-04 17:17:27 -0300

    refactor(app): simplify AppComponent structure and remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,15 @@
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ItemsListComponent } from "./components/items-list/items-list.component";
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { GroupProps } from './models/group';
-import { ApíService } from './services/apí.service';
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, CommonModule, ItemsListComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'cs-market'
-  filters: GroupProps[] = []
-
-  constructor(private apiService: ApíService) {}
-
-  ngOnInit(): void {
-    this.apiService.getGroups().subscribe(data => {
-      this.filters = data
-    })
-  }
-
-
 }
