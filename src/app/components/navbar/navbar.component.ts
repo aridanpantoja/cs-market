22583e0db55026ff1f0d8653b1cd3b91d8172279
Aridan <aridanpantoja@gmail.com>
2024-12-04 17:23:04 -0300

    feat(navbar): update navbar layout and integrate Cart component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,22 +1,22 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
+import { CartComponent } from '../cart/cart.component'
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CartComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   links = [
-    { label: 'Produtos', url: '/products', authRequired: false },
-    { label: 'Sobre', url: '/about', authRequired: false },
-    { label: 'Dashboard', url: '/dashboard', authRequired: true },
+    { label: 'Produtos', url: '/products' },
+    { label: 'Sobre', url: '/about' },
+    { label: 'Dashboard', url: '/dashboard' },
   ]
 
   isMenuOpen = false
-  isAuthenticated = false
 
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen
